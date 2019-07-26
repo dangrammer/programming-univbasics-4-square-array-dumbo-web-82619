c9aca93c9a8e4d9906335e8fb447ca3203568d6a
Dan Romans <danieljromans@gmail.com>
2019-07-26 02:40:56 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,8 +2,9 @@ def square_array(array)
   counter = 0 
   
   while counter < array.length do 
+  array[counter] ** 2  
   counter += 1 
   end
   
-  return array[counter] ** 2
+  return array.sort
 end
